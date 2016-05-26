cad1022c8abb6dc89513105515caccbde879a93b
Thyiad <1520583107@qq.com>
2016-05-26 18:40:04 +0800

    测试工具添加根据设计草图构造mongo数据；新增markdown笔记、常用html代码块

--- javascript 测试工具/ActionHandler.js
+++ javascript 测试工具/ActionHandler.js
@@ -7,6 +7,93 @@ function $(e)
 	return document.getElementById(e);
 }
 
+function log(txt,clear){
+	$('logArea').value = clear?txt:($('logArea').value+'\n'+txt);
+}
+
+String.prototype.trimStart=function(){
+	return this.replace(/^\s+/gi,'');
+}
+String.prototype.trimEnd=function(){
+	return this.replace(/\s+$/gi,'');
+}
+
+function createMongoData(){
+	var result = {};
+	var lineArray = $('sourceArea').value.split('\n');
+	
+	var levelSpaceLen = 3;
+	var regexEnd = /\s+$/gi;
+	var regexStart=/^\s+/gi;
+	var regexType=/\([^)]+\)/gi;
+	
+	var levelObj={
+		l0:result,
+	}
+	lineArray.forEach(function(item,idx){
+		var level = getLevel(item);
+		var type = '';
+		
+		var trimAll = item.trim();
+		var typeStr = regexType.exec(trimAll);
+		
+		if(typeStr){
+			type=typeStr[0].substring(1,typeStr[0].length-1)
+		}
+		
+		var listItem = type.split('/');
+		
+		var current = null;
+		
+		trimAll = trimAll.replace(regexType,'');
+		if(trimAll === 'status'){
+			console.log(111);
+		}
+		// 类型是下拉，则设置为下拉的随机项
+		if(listItem.length > 1){
+			var itemIdx = Math.floor(Math.random()*listItem.length+0);
+			current = listItem[itemIdx];
+		// 类型是数组
+		}else if(type === 'array'){
+			current=[];
+		// 没有类型、有子属性，设置为对象
+		}else if(getLevel(lineArray[idx+1]) > level){
+			current= {};
+		// 没有类型、没有子属性就设置为相同的文本
+		}else {
+			current = trimAll;
+		}
+		
+		if(Object.prototype.toString.call(current) === "[object Array]"){
+			if(getLevel(lineArray[idx+1]) > level){
+				current.push({});
+			}else{
+				current.push=(trimAll);
+			}
+		}
+		
+		var levelPre = levelObj['l'+(level-1).toString()];
+		if(Object.prototype.toString.call(levelPre) === "[object Array]"){
+			levelPre=levelPre[0];
+		}
+		
+		levelPre[trimAll]= levelObj['l'+level.toString()] = current;
+	})
+	
+	log(JSON.stringify(result),true);
+	
+	function getLevel(str){
+		if(str === null || str === undefined) return 1;
+		
+		var levelSpace = str.trimEnd().match(regexStart);
+		if(levelSpace){
+			return (levelSpace[0].length/levelSpaceLen)+1;
+		}else{
+			return 1;
+		}
+	}
+}
+
 function executeCode()
 {
     try
@@ -18,7 +105,7 @@ function executeCode()
     }
     catch(exception)
     {
-        $('logArea').value=exception;
+		log(exception);
     }
 }
 
